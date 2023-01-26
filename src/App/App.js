786dbdbb273e2a12d69436b9c2746c322bb5afc7
TIit0 <viverlin7@gmail.com>
2023-01-26 13:33:28 -0600

    before modal and adjusting button

--- src/App/App.js
+++ src/App/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Header from '../components/Header/Header';
 import Hero from '../components/Hero/Hero';
+import MyWork from '../components/MyWork/MyWork';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <MyWork />
     </div>
   );
 }
